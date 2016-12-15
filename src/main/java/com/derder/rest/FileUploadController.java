2f5637e73fea357f80fa9655043a3a6fa7d71b72
zhaolei <zhaolei@chinadrtv.com>
2016-12-16 00:12:59 +0800

    init

---
+++ src/main/java/com/derder/rest/FileUploadController.java
@@ -0,0 +1,37 @@
+package com.derder.rest;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhaolei
+ * Date: 16-12-15
+ * Time: 下午11:20
+ */
+@RestController
+public class FileUploadController {
+    @RequestMapping(value="/api/upload", method= RequestMethod.POST)
+    public @ResponseBody
+    String handleFileUpload(@RequestParam("name") String name,
+                                                 @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
+}
